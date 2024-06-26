01ebef6379722fd92ad97526b18a70d6bdb171e3
Jose Cerrato <josecerrato675@gmail.com>
2024-06-25 19:39:19 -0600

    First Commit

---
+++ src/views/auth/LoginView.tsx
@@ -0,0 +1,106 @@
+import { useForm } from "react-hook-form";
+import { UserLoginForm } from "@/types/index";
+import ErrorMessage from "@/components/ErrorMessage";
+import { Link, useNavigate } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
+import { authenticateUser } from "@/api/AuthAPI";
+import { toast } from "react-toastify";
+
+export default function LoginView() {
+  const initialValues: UserLoginForm = {
+    email: "",
+    password: "",
+  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ defaultValues: initialValues });
+
+  const navigate = useNavigate()
+
+  const { mutate } = useMutation({
+    mutationFn: authenticateUser,
+    onError: (error) => {
+      toast.error(error.message);
+    },
+    onSuccess: () => {
+      navigate('/')
+    },
+  });
+
+  const handleLogin = (formData: UserLoginForm) => mutate(formData);
+
+  return (
+    <>
+      <h1 className="text-5xl font-black text-white">Login</h1>
+      <p className="text-2xl font-light text-white mt-5">
+        Start planning your projects {""}
+        <span className=" text-fuchsia-500 font-bold">
+          {" "}
+          by logging in on this form
+        </span>
+      </p>
+      <form
+        onSubmit={handleSubmit(handleLogin)}
+        className="space-y-8 p-10 mt-10 bg-white"
+        noValidate
+      >
+        <div className="flex flex-col gap-5">
+          <label className="font-normal text-2xl">Email</label>
+
+          <input
+            id="email"
+            type="email"
+            placeholder="Registration Email"
+            className="w-full p-3  border-gray-300 border"
+            {...register("email", {
+              required: "Email is requied",
+              pattern: {
+                value: /\S+@\S+\.\S+/,
+                message: "Invalid Email",
+              },
+            })}
+          />
+          {errors.email && <ErrorMessage>{errors.email.message}</ErrorMessage>}
+        </div>
+
+        <div className="flex flex-col gap-5">
+          <label className="font-normal text-2xl">Password</label>
+
+          <input
+            type="password"
+            placeholder="Registration Password"
+            className="w-full p-3  border-gray-300 border"
+            {...register("password", {
+              required: "Password is required",
+            })}
+          />
+          {errors.password && (
+            <ErrorMessage>{errors.password.message}</ErrorMessage>
+          )}
+        </div>
+
+        <input
+          type="submit"
+          value="Sign In"
+          className="bg-fuchsia-600 hover:bg-fuchsia-700 w-full p-3  text-white font-black  text-xl cursor-pointer"
+        />
+      </form>
+      <nav className=" mt-10 flex flex-col space-y-4">
+        <Link
+          className=" text-center text-gray-300 font-normal"
+          to="/auth/register"
+        >
+          You do not have an account? Sign Up
+        </Link>
+        <Link
+          className=" text-center text-gray-300 font-normal"
+          to="/auth/forgot-password"
+        >
+          Did you forget your password? Reset
+        </Link>
+      </nav>
+    </>
+  );
+}
